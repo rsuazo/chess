d89021291dad767abf66ab231ca731a3c8be9deb
rsuazo <rsuazo2@gmail.com>
2020-03-11 14:56:19 -0700

    remove 'clear' word from file

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,3 +1,3 @@
 # require_relative "./chess/core_extensions.rb"
 lib_path = File.expand_path(File.dirname(__FILE__))
-Dir[lib_path + "/chess/**/*.rb"].each { |file| require file }clear
+Dir[lib_path + "/chess/**/*.rb"].each { |file| require file }
