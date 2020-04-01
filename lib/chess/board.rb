257f0798dc2edef5a051a82f18efde5842311892
rsuazo <rsuazo2@gmail.com>
2020-04-01 14:15:57 -0700

    updated some stuff

--- lib/chess/board.rb
+++ lib/chess/board.rb
@@ -393,31 +393,5 @@ class Board
     end
     false
   end
-   
-  def winning_position_values(winning_position)
-    winning_position.map { |cell| cell.value }
-  end
   
-  def winning_positions
-    grid + # rows
-    grid.transpose + # columns
-    diagonals # two diagonals
-  end
-  
-  def diagonals
-    [
-      [get_cell(0, 0), get_cell(1, 1), get_cell(2, 2), get_cell(3, 3), get_cell(4, 4), get_cell(5, 5)],
-      [get_cell(1, 0), get_cell(2, 1), get_cell(3, 2), get_cell(4, 3), get_cell(5, 4), get_cell(6, 5)],
-      [get_cell(2, 0), get_cell(3, 1), get_cell(4, 2), get_cell(5, 3), get_cell(6, 4)],
-      [get_cell(3, 0), get_cell(4, 1), get_cell(5, 2), get_cell(6, 3)],
-      [get_cell(0, 1), get_cell(1, 2), get_cell(2, 3), get_cell(3, 4), get_cell(4, 5)],
-      [get_cell(0, 2), get_cell(1, 3), get_cell(2, 4), get_cell(3, 5)],
-      [get_cell(6, 0), get_cell(5, 1), get_cell(4, 2), get_cell(3, 3), get_cell(2, 4), get_cell(1, 5)],
-      [get_cell(5, 0), get_cell(4, 1), get_cell(3, 2), get_cell(2, 3), get_cell(1, 4), get_cell(0, 5)],
-      [get_cell(4, 0), get_cell(3, 1), get_cell(2, 2), get_cell(1, 3), get_cell(0, 4)],
-      [get_cell(3, 0), get_cell(2, 1), get_cell(1, 2), get_cell(0, 3)],
-      [get_cell(6, 1), get_cell(5, 2), get_cell(4, 3), get_cell(3, 4), get_cell(2, 5)],
-      [get_cell(6, 2), get_cell(5, 3), get_cell(4, 4), get_cell(3, 5)]
-    ]
-  end
 end
