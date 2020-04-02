9c61fea208a6f8bc777d3b7873a40476b8ab4c95
rsuazo <rsuazo2@gmail.com>
2020-04-02 11:19:02 -0700

    update game class to include play engine

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,3 +1,11 @@
 require_relative "./chess/core_extensions.rb"
 lib_path = File.expand_path(File.dirname(__FILE__))
 Dir[lib_path + "/chess/**/*.rb"].each { |file| require file }
+
+
+puts "Welcome to Chess"
+robert = Player.new({color: "X", name: "robert"})
+tiffany = Player.new({color: "O", name: "tiffany"})
+players = [robert, tiffany]
+game = Game.new(players)
+game.play
--- lib/chess/game.rb
+++ lib/chess/game.rb
@@ -30,5 +30,26 @@ class Game
 
     [letter_to_num[human_move[0].downcase], human_move[1].to_i - 1]
   end
+
+
+  def play
+    puts "#{current_player.name} has randomly been selected as the first player"
+    while true
+      board.formatted_grid
+      puts ""
+      puts solicit_move
+      x, y = get_move
+      board.set_cell(x, y, current_player.color)
+      if board.game_over
+        puts game_over_message
+        board.formatted_grid
+        return
+      else
+        switch_players
+      end
+    end
+  end
   
 end
+
+
